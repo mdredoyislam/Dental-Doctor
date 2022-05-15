2b4710fc9444bdc122ea033d8ae5db7aaa36ccd9
Md Redoy Islam <mdredoyislam84@gmail.com>
2022-05-15 23:20:54 +0600

    Dental Doctor Template

---
+++ assets/js/custom.js
@@ -0,0 +1,38 @@
+$(document).ready(function(){
+    $('.blog').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:false,
+        autoplay:true,
+        dots : false,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:3
+            }
+        }
+    });
+    $('.owl-testimonials').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:false,
+        autoplay:true,
+        dots : false,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:3
+            }
+        }
+    });
+});
